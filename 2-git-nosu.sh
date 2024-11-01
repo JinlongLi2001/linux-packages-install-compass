7f7d592c15fa6d8e423bdfb79e9297d90b03cbaa
JinlongLi <ljl66623@mail.nwpu.edu.cn>
2024-11-01 17:17:14 +0800

    add README

--- 2-git-nosu.sh
+++ 2-git-nosu.sh
@@ -1,9 +1,9 @@
 ##git installed
-git config --global user.name “Jiuxiaoyun”
-git config --global user.email “3292913147@qq.com”
+git config --global user.name JinlongLi2001
+git config --global user.email ljl66623@gmail.com
 git config --global --list
-ssh-keygen -t rsa -C "3292913147@qq.com"
+ssh-keygen -t rsa -C "ljl66623@gmail.com"
 cat /home/ljl/.ssh/id_rsa.pub
 #粘贴公钥
-read -p "gitee粘贴公钥，完成后按"Enter""
+read -p "gitee粘贴公钥, 完成后按"Enter""
 ssh -T git@gitee.com      #粘贴后运行本行
---
+++ README.md
@@ -0,0 +1,7 @@
+# Linux-packages-install-compass
+
+This repository is written for installing packages that may occurs errors while follow official installation documentation.
+
+For only linux, especially Ubuntu, by usage of the author.
+
+## Just read the name and contains of markdowns and shells.
